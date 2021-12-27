ec3c01102ef46fff78ce7ebde77447ab74d16651
Isay <isay2005u@mail.ru>
2021-12-27 14:50:12 +0300

    init

---
+++ Episode.js
@@ -0,0 +1,14 @@
+import episodes from "./episodes.js"
+
+class Episode{
+  constructor(title, description, words){
+    this.title = title
+    this.description = description
+    this.words = words
+  }
+  
+}
+
+
+
+console.log(episodes);
---
+++ episodes.js
@@ -0,0 +1,7 @@
+export const episodes = [
+    {
+        title: "Monica brings a new neighbour",
+        description: '.lfopsmdffds.',
+        words: ['to grab the spoon=оклиматься, вернуться к жизни', 'to ask out=приглашать провести время']
+    }
+]
